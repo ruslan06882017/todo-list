af482a525d7df9e7c6ca372dd32236fb45e2efda
Ruslan Gusseinov <r.gusseinov@mail.ru>
2020-04-10 23:39:40 +0600

    Started project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <form>
+          <h1> Todo list Project </h1>
+          <hr/>
+          <input type="text" className="todo-item"/>
+        </form>
+    </div>
+  );
+}
+
+export default App;
